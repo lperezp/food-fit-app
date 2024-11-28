46f88753c250834371d758c7ad39c6e8d7ffbeb8
Luis Eduardo <luiseperezp.10@gmail.com>
2024-11-28 03:26:53 -0500

    feat(app): Add header component and search page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: SearchPageComponent
+    }
+];
---
+++ src/app/pages/search-page/search-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
+
+@Component({
+  selector: 'app-search-page',
+  imports: [HeaderComponent],
+  templateUrl: './search-page.component.html',
+  styleUrl: './search-page.component.scss'
+})
+export class SearchPageComponent {
+
+}
