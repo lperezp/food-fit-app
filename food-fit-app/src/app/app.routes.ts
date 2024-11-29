1b72913236e60cdfd36e2633128edab6e479ed77
Luis Eduardo <luiseperezp.10@gmail.com>
2024-11-29 02:06:17 -0500

    feat(app): add home page component and update routing

--- food-fit-app/src/app/app.routes.ts
+++ food-fit-app/src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [
     {
         path: '',
+        component: HomePageComponent
+    },
+    {
+        path: 'search',
         component: SearchPageComponent
     }
 ];
