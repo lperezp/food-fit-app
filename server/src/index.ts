470dd1e578e533b0ae27b667162a41e3ee2e744c
Luis Eduardo <luiseperezp.10@gmail.com>
2024-11-29 02:35:11 -0500

    feat(server): update food suggestion schemas and enhance AI flow

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,28 +3,11 @@ require('dotenv').config();
 // import the Genkit and Google AI plugin libraries
 import { gemini15Flash, googleAI } from '@genkit-ai/googleai';
 import { z, genkit } from 'genkit';
+import { inputSchema, outputFoodItemSchema, outputListFoodItemSchema } from '../schemas/schema';
 
-const outputFoodItemSchema = z.object({
-    name: z.string(),
-    description: z.string(),
-    ingredients: z.array(z.string()),
-    nutritional_information: z.object({
-        cal: z.number(),
-        carbohydrates: z.number(),
-        fats: z.number(),
-        sodium: z.number(),
-        cholesterol: z.number(),
-        proteins: z.number(),
-    }),
-    preparation_time: z.string(),
-    level: z.string(),
-    preparation: z.array(z.string()),
-});
 
-const inputSchema = z.object({
-    ingredient: z.string(),
-    quantity_people: z.number()
-});
+
+
 
 // configure a Genkit instance
 const ai = genkit({
@@ -68,7 +51,7 @@ export const foodSuggestionFlow = ai.defineFlow(
 export const listFoodsSuggestionFlow = ai.defineFlow(
     {
         name: 'listFoodsSuggestionFlow',
-        outputSchema: z.array(outputFoodItemSchema),
+        outputSchema: outputListFoodItemSchema,
     },
     async () => {
         const { output } = await ai.generate({
@@ -76,15 +59,11 @@ export const listFoodsSuggestionFlow = ai.defineFlow(
             prompt: `Eres el asistente de inteligencia artificial más conocedor del rubro gastronómico.
             Genere un lista de 4 recetas para una persona que quiere alimentarse de forma saludable.
             En la matriz del las recetas, coloque las recetas como lo haría un recetario de comida.
-
             Dé a cada receta una descripción únicos.
-
             Las recetas deben ser saludables y equilibradas. Además que sean para 4 personas.
-
             Las recetas tiene que estar en español.
-
             Limite las descripciones de las recetas a 7 palabras.`,
-            output: { schema: z.array(outputFoodItemSchema) }
+            output: { schema: outputListFoodItemSchema }
         });
         if (output == null) {
             throw new Error("Response doesn't satisfy schema.");
