6f6632c66ae9047be834f9d2fcf5cc74cd79ca56
Luis Eduardo <luiseperezp.10@gmail.com>
2025-11-14 18:40:21 -0500

    feat: add food suggestion flow with prohibited foods

--- server/functions/src/index.local.ts
+++ server/functions/src/index.local.ts
@@ -3,13 +3,20 @@ require('dotenv').config();
 import { z, genkit } from 'genkit';
 import { gemini20Flash001, imagen3, vertexAI } from '@genkit-ai/vertexai';
 import { outputFoodItemSchema } from './schemas/output-food-item.schema';
-import { inputSchema } from './schemas/input.schema';
+import { inputSchema, inputSchemaWithProhibitedFood } from './schemas/input.schema';
+import * as admin from 'firebase-admin';
 
 const ai = genkit({
     plugins: [
         vertexAI({ location: 'us-central1' }),
     ],
 });
+
+if (!admin.apps.length) {
+    admin.initializeApp();
+}
+
+export const db = admin.firestore();
 
 export const foodSuggestionFlow = ai.defineFlow(
     {
@@ -91,5 +98,50 @@ export const generateImageFoodFlow = ai.defineFlow(
         }
 
         return response.message.content[0].media;
+    }
+);
+
+export const foodSuggestionWithProhibitedFoodFlow = ai.defineFlow(
+    {
+        name: 'foodSuggestionWithProhibitedFoodFlow',
+        inputSchema: inputSchemaWithProhibitedFood,
+        outputSchema: z.array(outputFoodItemSchema),
+    },
+    async (payload) => {
+        const userDoc = await db.collection('prohibited-food').doc(payload.userId).get();
+        const prohibitedFoods = userDoc.exists ? userDoc.data()?.foods : [];
+
+        const prohibitedFoodsContext = prohibitedFoods.length > 0
+            ? `El usuario es alérgico a: ${prohibitedFoods.join(', ')}. **ASEGÚRATE DE EXCLUIR ESTOS INGREDIENTES. EN EL CASO TE INDIQUE UN ALIMENTO DE LA LISTA COMO ALIMENTO PRINCIPAL, NO GENERES NINGUNA RECETA.**`
+            : 'No hay alergias conocidas.';
+
+        const { output } = await ai.generate({
+            model: gemini20Flash001,
+            prompt: `
+            Eres el asistente de inteligencia artificial más conocedor del rubro gastronómico.
+            Genere un lista de 4 recetas para una persona que quiere alimentarse de forma saludable.
+            En la matriz del las recetas, coloque las recetas como lo haría un recetario de comida.
+
+            Las recetas deben contener ${payload.ingredient} como ingrediente principal.
+
+            Dé a cada receta una descripción únicos.
+
+            Las recetas deben ser saludables y equilibradas. Además que sean para ${payload.quantity_people} personas.
+
+            Instrucciones de seguridad: ${prohibitedFoodsContext}
+
+            Las recetas tiene que estar en español.
+
+            Limite las descripciones de las recetas a 7 palabras.
+            `,
+            config: {
+                temperature: 0.2,
+            },
+            output: { schema: z.array(outputFoodItemSchema) }
+        });
+        if (output == null) {
+            throw new Error("Response doesn't satisfy schema.");
+        }
+        return output;
     }
 );
