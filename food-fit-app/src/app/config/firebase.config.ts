4d6b2bedb69617224ae20c872370804a818e24e9
Luis Eduardo <luiseperezp.10@gmail.com>
2025-11-16 08:59:48 -0500

    feat: update Firebase configuration to use environment variables

--- food-fit-app/src/app/config/firebase.config.ts
+++ food-fit-app/src/app/config/firebase.config.ts
@@ -1,19 +1,9 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-
-// Tu configuración de Firebase
-// Reemplaza estos valores con los de tu proyecto Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBEv4xmxTp0uNTp-K3QvQUxEAjQza2PXGg",
-    authDomain: "food-fit-2024.firebaseapp.com",
-    projectId: "food-fit-2024",
-    storageBucket: "food-fit-2024.firebasestorage.app",
-    messagingSenderId: "492381764039",
-    appId: "1:492381764039:web:f59812888c06df761401e2"
-};
+import { environment } from '../../environments/environment';
 
 // Inicializar Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(environment.firebaseConfig);
 
 // Inicializar Firebase Auth y obtener una referencia al servicio
 export const auth = getAuth(app);
